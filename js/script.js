a8ebf203207932b80e289f8fef6301f5c8d15125
Alex <magdubor@gmail.com>
2022-03-07 14:33:03 +0200

    task 1
    task 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-let number = 5;
-const leftBorderWidth = 1;
+"use strict"
+
+const numberOfFilms = +prompt('Скільки фільмів ви уже переглянули?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один із останніх переглянутих фільмів?', ''),
+      b = prompt('Оцініть його', ''),
+      c = prompt('Один із останніх переглянутих фільмів?', ''),
+      d = prompt('Оцініть його', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
